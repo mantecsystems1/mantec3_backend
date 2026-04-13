402e9a5e89539fcd653c519d3f53340abcd6860c
lemuelpires <107230579+lemuelpires@users.noreply.github.com>
2026-04-12 21:22:16 -0300

    validators em produtos

--- src/catalogo/produtos/dto/create-produto.dto.ts
+++ src/catalogo/produtos/dto/create-produto.dto.ts
@@ -1,8 +1,25 @@
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
+
 export class CreateProdutoDto {
+  @IsString()
   empresaId: string;
+
+  @IsString()
   nome: string;
+
+  @IsOptional()
+  @IsString()
   descricao?: string;
+
+  @IsOptional()
+  @IsString()
   codigoInterno?: string;
+
+  @IsOptional()
+  @IsString()
   precoVenda?: string;
+
+  @IsOptional()
+  @IsBoolean()
   ativo?: boolean;
 }
